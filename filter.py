bc07055fc1d1dd98c5f6aafa42e9169ad85d79c7
Raphael Schweikert <any@sabberworm.com>
2016-11-08 17:45:10 +0100

    Update for mitmproxy 0.18.2 and python 3

--- filter.py
+++ filter.py
@@ -1,7 +1,7 @@
 #coding=utf-8
 from subprocess import Popen
 from subprocess import check_output
-from libmproxy.protocol.http import decoded
+from mitmproxy.models.http import HTTPResponse
 import os
 import re
 import glob
@@ -15,7 +15,7 @@ artwork_mapping = {}
 request_mapping = {}
 previous_location = None
 
-def start(context, argv):
+def start():
     global previous_location
     out = check_output("scselect", universal_newlines=True)
     sets = out.splitlines()[1:]
@@ -31,52 +31,51 @@ def start(context, argv):
         break
     Popen(("scselect", "CoverArtProxy"))
 
-def done(context):
+def done():
     if previous_location is not None:
         Popen(("scselect", previous_location))
 
 
-def request(context, flow):
+def request(flow):
     if flow.request.path.startswith("/WebObjects/MZStoreServices.woa/wa/coverArtMatch"):
-        return artwork_query_request(context, flow)
+        return artwork_query_request(flow)
 
 
-def artwork_query_request(context, flow):
-    query = flow.request.get_query()
+def artwork_query_request(flow):
+    query = flow.request.query
     if not ("an" in query):
         # This is for an album downloaded from iTunes, we’ve only got an id for artist and album.
         # Make sure it comes back with an error so iTunes tries again with names for both.
-        query['a'] = ['B8jOURYtHxYv',]
-        query['p'] = ['Qpfz0SDV4Xgr',]
+        query['a'] = 'B8jOURYtHxYv'
+        query['p'] = 'Qpfz0SDV4Xgr'
     else:
-        artist = query["aan"][0] if ("aan" in query) else query["an"][0]
-        album = query["pn"][0]
+        artist = query["aan"] if ("aan" in query) else query["an"]
+        album = query["pn"]
         request_mapping[flow.request] = (artist, album)
         # We can’t intercept the response to this request as there’s some kind of checksum in it.
         # But we can intercept the response to the image being served for this request.
         # So we need to use something we’re sure will yield a correct response.
-        query["aan"] = query["an"] = ["Britney Spears",]
-        query["pn"] = ["Britney",]
-        query["dummy"] = [artist, album]
-    flow.request.set_query(query)
+        query["aan"] = query["an"] = 'Britney Spears'
+        query["pn"] = 'Britney'
+        query.set_all('dummy', [artist, album])
 
 
-def response(context, flow):
+def response(flow):
     if flow.request.path.startswith("/WebObjects/MZStoreServices.woa/wa/coverArtMatch"):
-        return artwork_query_response(context, flow)
+        return artwork_query_response(flow)
     if flow.request.url in artwork_mapping:
-        return artwork_serve_response(context, artwork_mapping[flow.request.url], flow)
+        return artwork_serve_response(artwork_mapping[flow.request.url], flow)
 
 
-def artwork_query_response(context, flow):
-    response = flow.response
-    with decoded(response):
-        artwork_url = response.content.partition('<dict>')[2]
-        artwork_url = artwork_url.partition('http')[2]
-        artwork_url = artwork_url.partition('</string>')[0]
-        artwork_url = 'http' + artwork_url.strip()
-        artwork_mapping[artwork_url] = request_mapping[flow.request]
-        print artwork_url, 'for', request_mapping[flow.request]
+def artwork_query_response(flow):
+    flow.response.decode()
+    artwork_url = flow.response.content.partition('<dict>')[2]
+    artwork_url = artwork_url.partition('http')[2]
+    artwork_url = artwork_url.partition('</string>')[0]
+    artwork_url = 'http' + artwork_url.strip()
+    artwork_mapping[artwork_url] = request_mapping[flow.request]
+    flow.response.encode('gzip')
+    print(artwork_url, 'for', request_mapping[flow.request])
 
 def artwork_folder(artist, album):
     path = os.path.join(MY_ALBUM_ART_FOLDER, artist, album)
@@ -94,27 +93,26 @@ def serve_artwork_in_folder(folder, flow):
         extension = file.rpartition('.')[2].lower()
         if not extension in EXTENSIONS:
             continue
-        print "Outputting file " + file
+        print("Outputting file " + file)
         response = flow.response
-        with decoded(response):
-            with open(file) as fptr:
-                response.headers["Content-Type"] = [EXTENSIONS[extension],]
-                response.content = fptr.read()
-                response.code = 200
-                return True
+        response.decode()
+        with open(file) as fptr:
+            response.headers["Content-Type"] = EXTENSIONS[extension]
+            response.content = fptr.read()
+            response.code = 200
+            return True
     return False
 
 
-def artwork_serve_response(context, artwork_info, flow):
-    print "Trying to get artwork for", artwork_info[0], "–", artwork_info[1]
+def artwork_serve_response(artwork_info, flow):
+    print("Trying to get artwork for", artwork_info[0], "–", artwork_info[1])
     if serve_artwork_in_folder(artwork_folder(artwork_info[0], artwork_info[1]), flow):
-        print "Found exact match"
+        print("Found exact match")
         return
     if serve_artwork_in_folder(artwork_folder("Compilation", artwork_info[1]), flow):
-        print "Found compilation match"
+        print("Found compilation match")
         return
-    print "No match found"
+    print("No match found")
     response = flow.response
-    with decoded(response):
-        response.content = ''
-        response.code = 404
+    response.content = ''
+    response.code = 404
